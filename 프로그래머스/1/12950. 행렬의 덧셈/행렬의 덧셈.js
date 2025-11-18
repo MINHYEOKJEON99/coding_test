c15cc7307737cc7160d3075ec869a4026de85f0a
전민혁 <133513404+MINHYEOKJEON99@users.noreply.github.com>
2025-11-18 22:36:24 +0900

    [level 1] Title: 행렬의 덧셈, Time: 10.80 ms, Memory: 67.7 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,12 +1,12 @@
 function solution(arr1, arr2) {
-    let answer = [];
-    let el = [];
-    for (let i = 0; i < arr1.length; i++) {
-        for (let j = 0; j < arr1[i].length; j++) {
-            el.push(arr1[i][j] + arr2[i][j])
+    let answer = []
+    for(let i = 0; i < arr1.length; i++) {
+        let arr = []
+        for(let j = 0; j < arr1[i].length; j++) {
+            arr = [...arr, arr1[i][j] + arr2[i][j]]
         }
-        answer.push(el)
-        el = [];
+        answer = [...answer, arr]
     }
+    
     return answer
 }
