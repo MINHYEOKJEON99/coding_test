01759d3395ee93cc70e3bd3a59423aa6fedb6a2f
전민혁 <133513404+MINHYEOKJEON99@users.noreply.github.com>
2025-11-30 17:33:35 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,3 +1,3 @@
 function solution(array, commands) {
-    return commands.map((command) => array.slice(command[0] - 1,command[1]).sort((a,b) => a - b )[command[2] - 1])
+    return commands.map((arr) => array.slice(arr[0] - 1, arr[1]).sort((a,b) => a - b)[arr[2] - 1])
 }
